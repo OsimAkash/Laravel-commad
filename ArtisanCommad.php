a281bc8b1e4072f89e03f36a51bd022adfba1653
Osim Kumar Roy (Akash) <97542936+OsimAkash@users.noreply.github.com>
2025-01-26 12:34:12 +0600

    Update ArtisanCommad.php

--- ArtisanCommad.php
+++ ArtisanCommad.php
@@ -21,3 +21,21 @@ Ex: 100 fake data store database products table
                 'updated_at'      =>  now(),
             ]);
         }
+
+
+        Php Faker-Data Used Ex: 
+         $fake = Factory::create();
+
+        for ($i = 0; $i < 100; $i++) {
+            # code...
+                DB::table('products')->insert([
+                'name'           =>  $fake->realText(60),
+                'seler_name'     =>  $fake->name,
+                'description'    =>  $fake->realText(),
+                'price'          =>  (float) random_int(10, 1000),
+                'created_at'      =>  now(),
+                'updated_at'      =>  now(),
+            ]);
+        }
+php artisan migrate:fresh --seed
+                
